ca1f88a663e575f690da96402832879277608159
aldacelio <aldacelio@alu.ufc.br>
2025-06-02 09:20:48 -0300

    feat: adding classes for token configuration, processing and validation.

---
+++ backend/src/main/java/com/upgrade/backend/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.upgrade.backend.security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.upgrade.backend.repository.UserRepository;
+import com.upgrade.backend.service.JwtService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        var token = this.recoverToken(request);
+        
+        if (token != null) {
+            var email = jwtService.validateToken(token);
+            var user = userRepository.findByEmail(email).orElseThrow();
+            
+            var authentication = new UsernamePasswordAuthenticationToken(email, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
